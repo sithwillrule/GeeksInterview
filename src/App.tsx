51a27ad87604a78ddf25232f2ce374866e5b2af1
Erfan <erfanviva@gmail.com>
2021-12-31 19:03:58 +0330

    yo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Label from './Components/Box';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <Label/>    
       </header>
     </div>
   );
---
+++ src/Components/Box.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class LabelComponent extends Component{
+    render() {
+        return (
+		<div className="demo">
+	            <label className="label">Hello App</label>
+		</div>
+        )
+    }
+}
+export default LabelComponent;
