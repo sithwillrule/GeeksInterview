7881383c9981da41806e53760117fb391b0ee6c4
Erfan <erfanviva@gmail.com>
2021-12-31 11:01:32 +0330

    fdd

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-   
+        <img  className="App-logo" alt="logo" />
+        <p>
+          Edit ME<code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
